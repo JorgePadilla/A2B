5b6613ec8b4d23c40f46d873ca89c7eec0ed9599
root <root@DESKTOP-EQ2OFGT.localdomain>
2018-07-17 18:26:48 -0600

    fix default route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  default_url_options :host => "localhost:3000"
 end
